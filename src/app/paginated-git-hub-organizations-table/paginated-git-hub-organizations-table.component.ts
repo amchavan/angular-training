74e5690118ac0e3deba914bdc1cb6e67bbee34e8
amchavan <amchavan>
2021-02-12 16:41:10 +0100

    Unit 3: Added paginated table

---
+++ src/app/paginated-git-hub-organizations-table/paginated-git-hub-organizations-table.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {GitHubOrganization} from '../git-hub-organization';
+import {GitHubOrganizationsService} from '../git-hub-organizations.service';
+
+@Component({
+    selector: 'app-paginated-git-hub-organizations-table',
+    templateUrl: './paginated-git-hub-organizations-table.component.html',
+    styleUrls: ['./paginated-git-hub-organizations-table.component.css']
+})
+export class PaginatedGitHubOrganizationsTableComponent implements OnInit {
+
+    gitHubOrganizations: GitHubOrganization[];
+    currentPage = 1;    // Always start from the first page!
+    readonly pageSize = 10;
+
+    constructor( private gitHubOrganizationsService: GitHubOrganizationsService ) {
+    }
+
+    ngOnInit(): void {
+        this.loadOrganizationsPage( this.pageSize, this.currentPage );
+    }
+
+    private loadOrganizationsPage( pageSize: number, currentPage: number ): void {
+        this.gitHubOrganizationsService
+            .fetchOrganizationsPage( pageSize, currentPage )
+            .then(organizations => {
+                if (organizations) {
+                    this.gitHubOrganizations = organizations;
+                }
+            });
+    }
+
+    newCurrentPageEventHandler( newPageNumber: number ): void {
+        this.currentPage = newPageNumber;
+        this.loadOrganizationsPage( this.pageSize, this.currentPage );
+    }
+}
